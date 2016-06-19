a336ffa6e347e717b3fa394d7b4af206c71b7206
Cyrille Hamel <cyrille.hamel@kicknride.fr>
2016-06-19 11:42:29 +0200

    Correct text-domain error

--- functions.php
+++ functions.php
@@ -496,7 +496,7 @@ function knr_fonts_url() {
 		$subsets   = 'latin';
 
 		/* translators: If there are characters in your language that are not supported by Open Sans, translate this to 'off'. Do not translate into your own language. */
-		if ( 'off' !== _x( 'on', 'Russo One font: on or off', 'flatter' ) ) {
+		if ( 'off' !== _x( 'on', 'Russo One font: on or off', 'knr' ) ) {
 			$fonts[] = 'Russo One:400';
 		}
 
